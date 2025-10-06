e6121b65265d0ecee9068f4342434bf799122f46
Упольников Александр Александрович ИП-513 <sansan857@mail.ru>
2025-10-06 20:55:06 +0700

    Конец

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    float a[10];
+    float sum = 0;
+    float sr = 0;
+
+    for(int i = 0; i < 10; i++) {
+        scanf("%f", &a[i]);
+        sum = sum + a[i];
+    }
+
+    sr = sum / 10;
+    printf("%.2f\n", sr);
+
+    return 0;
+}
